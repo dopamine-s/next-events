988baa1ef6ed140320750aea1ddb1f0dd67ba5d1
dopamine-s <motor55005@gmail.com>
2023-04-06 15:16:20 +0200

    styling and adding buttons and icons

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,8 @@
-import Link from 'next/link';
+import Button from '../ui/button';
+import DateIcon from '../icons/date-icon';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
+import classes from './event-item.module.css';
 
 function EventItem({ item }) {
   const { image, title, date, location, id } = item;
@@ -10,21 +14,31 @@ function EventItem({ item }) {
   });
 
   const formattedAddress = location.replace(', ', '\n');
+  const exploreLink = `/events/${id}`;
 
   return (
-    <li>
+    <li className={classes.item}>
       <img src={'/' + image} alt={title} />
-      <div>
-        <h2>{title}</h2>
+      <div className={classes.content}>
         <div>
-          <time>{humanReadableDate}</time>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <DateIcon />
+            <time>{humanReadableDate}</time>
+          </div>
+          <div className={classes.address}>
+            <AddressIcon />
+            <address>{formattedAddress}</address>
+          </div>
+          <div className={classes.actions}>
+            <Button link={exploreLink}>
+              <span>Explore Event</span>
+              <span className={classes.icon}>
+                <ArrowRightIcon />
+              </span>
+            </Button>
+          </div>
         </div>
-        <div>
-          <address>{formattedAddress}</address>
-        </div>
-      </div>
-      <div>
-        <Link href={`/events/${id}`}>Explore Event</Link>
       </div>
     </li>
   );
